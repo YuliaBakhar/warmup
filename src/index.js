4c4a712b165b7520f78ee7657b2b45f9d8b11a97
Yulia <bakhar.yulia@gamil.com>
2019-02-20 10:51:50 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-module.exports = function warmup(temperature) {
-  temperature = 1.8 * temperature  + 32;
-  return temperature; 
-};
+module.exports = warmup = temperature => 1.8 * temperature + 32;
